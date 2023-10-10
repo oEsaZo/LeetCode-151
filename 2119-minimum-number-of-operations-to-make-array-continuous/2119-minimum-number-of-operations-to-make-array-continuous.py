39bc5e7793ac1d1cc37bad6fc9d28bedcca16884
Ehssas Samal <77456161+oEsaZo@users.noreply.github.com>
2023-10-10 19:31:48 +0530

    Time: 713 ms (54.6%), Space: 35.6 MB (26.97%) - LeetHub

---
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.py
@@ -0,0 +1,16 @@
+from typing import List
+import sys
+from bisect import bisect_right
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums = sorted(set(nums))
+        ans = sys.maxsize
+
+        for i, s in enumerate(nums):
+            e = s + n - 1
+            idx = bisect_right(nums, e)
+
+            ans = min(ans, n - (idx - i))
+        return ans
