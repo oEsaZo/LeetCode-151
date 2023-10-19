3405bf52c5f687b75f3d3db45ef4941454ae65fc
Ehssas Samal <77456161+oEsaZo@users.noreply.github.com>
2023-10-19 18:27:38 +0530

    Time: 49 ms (49.79%), Space: 16.2 MB (58.58%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        #checkin edge cases
+        if len(num1) == 0 or len(num2) == 0:
+            return '0'
+        if num1[0] == '0' or num2[0] == '0':
+            return '0'
+        #convert to integer
+        res1, res2 = 0,0
+        for d in num1:
+            res1 = res1 * 10 + (ord(d) - ord('0'))
+        for d in num2:
+            res2 = res2 * 10 + (ord(d) - ord('0'))
+        
+        res = res1 * res2
+        ans = ''
+        while res:
+            ans = ans + (chr(ord('0') + res%10))
+            res //= 10
+        return ans[::-1]
